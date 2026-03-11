f08849163a977c9a08eb930fc5cacd95b8603d59
itouyuuki <itou@nextreservation.jp>
2026-03-11 14:43:05 +0900

    tes2t

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $base="./"
 <section id="about">
     <div class="hgroup-con">
         <hgroup>
-            <p>Company3</p>
+            <p>Company6</p>
             <h2>会社概要</h2>
         </hgroup>
     </div>
